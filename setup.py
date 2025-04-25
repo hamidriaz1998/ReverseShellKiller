d430816dfd09677f3c46071b9b25fc9459b4ef18
Ahmed8881 <buttahmed2004@gmail.com>
2025-04-25 16:59:13 +0500

    Refactor setup.py to read requirements from requirements.txt and update package configuration

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="reverse-shell-killer",
     version="0.1.0",
     description="Tool to identify and terminate reverse shells",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="Ahmed", 
-    py_modules=["main"], 
+    author="Ahmed",
+    packages=find_packages() + ["revshell_detector"],
+    py_modules=["main"],
     python_requires=">=3.13",
-    install_requires=[
-        "psutil>=7.0.0",
-    ],
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "reverse-shell-killer=main:main",  # Creates a command-line executable
+            "reverse-shell-killer=main:main",
         ],
     },
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Security",
+    ],
 )
